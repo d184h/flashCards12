ee0b632171e34fc98d87d819e9c7b85982f0aaa1
Roman Dianov <20m1k54nh@mail.ru>
2023-06-02 10:37:55 +0300

    questitem

--- routes/views/cards.routes.js
+++ routes/views/cards.routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { Card } = require('../../db/models');
 const CardsList = require('../../components/CardsList');
+const { Quest } = require('../../db/models');
+const QuestionItem = require('../../components/QuestionItem');
 
 router.get('/', async (req, res) => {
   try {
@@ -8,6 +10,16 @@ router.get('/', async (req, res) => {
     res.send(res.renderComponent(CardsList, { title: 'Темы', cards }));
   } catch (error) {
     res.json(error.message);
+  }
+});
+
+router.get('/questions/:cardId', async (req, res) => {
+  try {
+    const { cardId } = req.params;
+    const question = Quest.findOne({ where: { id: cardId } });
+    res.send(res.renderComponent(QuestionItem, { title: 'Вопросы', question }));
+  } catch ({ message }) {
+    res.json({ message });
   }
 });
 
