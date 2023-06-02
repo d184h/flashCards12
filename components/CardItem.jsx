2791aca551bbd4d236e90bb7da2df69fde575624
Roman Dianov <20m1k54nh@mail.ru>
2023-06-02 10:01:55 +0300

    rega

--- components/CardItem.jsx
+++ components/CardItem.jsx
@@ -2,8 +2,10 @@ const React = require('react');
 
 function CardItem({ card }) {
   return (
-    <div className='card text-bg-primary mb-3' style={{ maxWidth: '18rem' }}>
-      <div className='card-header'>{card.name}</div>
+    <div className="card text-bg-primary mb-3" style={{ maxWidth: '18rem' }}>
+      <button className="card-header">
+        <a href="/cards/">{card.name}</a>
+      </button>
       {/* <div className='card-body'></div> */}
     </div>
   );
